d20754aa67418132dbcb7f2e8e59047a44cd473c
Igor Mosyagin <i.mosyagin@lamoda.ru>
2019-04-29 00:40:48 +0300

    Fix player moving diagonally

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -13,13 +13,15 @@ func _ready():
 func control(delta):
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
-		velocity = Vector2(speed, 0)
+		velocity += Vector2(speed, 0)
 	if Input.is_action_pressed("ui_left"):
-		velocity = Vector2(-speed, 0)
+		velocity += Vector2(-speed, 0)
 	if Input.is_action_pressed("ui_down"):
-		velocity = Vector2(0, speed)
+		velocity += Vector2(0, speed)
 	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2(0, -speed)	
+		velocity += Vector2(0, -speed)
+	
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
 	if not alive:
