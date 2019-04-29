9289610427af6c6eb3b4acca332f1bfc5f7ff97a
Igor Mosyagin <i.mosyagin@lamoda.ru>
2019-04-29 23:47:36 +0300

    Speed up mob when close to the player

--- Characters/Mob.gd
+++ Characters/Mob.gd
@@ -1,18 +1,23 @@
 extends KinematicBody2D
 
-export (int) var speed
+var speed = 70
 
-var velocity = Vector2()
+export (int) var walk_speed
+export (int) var run_speed
+export (int) var detect_radius
+
 var alive = true
 
 onready var parent = get_parent()
 
 func _ready():
-	pass
+	$DetectRadius/CollisionShape2D.shape.radius = detect_radius
+	speed = walk_speed
 
 func control(delta):
 	if parent is PathFollow2D:
 		parent.set_offset(parent.get_offset() + speed * delta)
+		position = Vector2()
 	else:
 		pass
 
@@ -28,3 +33,11 @@ func _physics_process(delta):
 #		return
 #	control(delta)
 #	move_and_slide(velocity)
+
+func _on_DetectRadius_body_entered(body):
+	if body.name == "Player":
+		speed = run_speed
+
+func _on_DetectRadius_body_exited(body):
+	if body.name == "Player":
+		speed = walk_speed
