58bbdf1b8c46302b48dee3b7b04c5e6bc52496e9
Igor Mosyagin <i.mosyagin@lamoda.ru>
2019-04-29 22:26:21 +0300

    Add all-tiles tileset and fix tile map generator

--- terrain/TileMapMaker.gd
+++ terrain/TileMapMaker.gd
@@ -10,30 +10,14 @@ func grab_terrain_tiles():
     var tex_width = texture.get_width() /  tile_size.x
     var tex_height = texture.get_height() / tile_size.y
     var ts = TileSet.new()
-    for x in range(23):
-        for y in range(6):
+    for x in range(tex_width):
+        for y in range(tex_height):
             var region = Rect2(x * tile_size.x, y * tile_size.y,
                                tile_size.x, tile_size.y)
-            var id = x * 10 + y
+            var id = x + y * tex_width
             ts.create_tile(id)
             ts.tile_set_texture(id, texture)
             ts.tile_set_region(id, region)
 
-    for x in range(8, 23):
-        for y in [6, 7]:
-            var region = Rect2(x * tile_size.x, y * tile_size.y,
-                               tile_size.x, tile_size.y)
-            var id = x * 10 + y
-            ts.create_tile(id)
-            ts.tile_set_texture(id, texture)
-            ts.tile_set_region(id, region)
-    for x in range(8, 16):
-        var y = 8
-        var region = Rect2(x * tile_size.x, y * tile_size.y,
-                           tile_size.x, tile_size.y)
-        var id = x * 10 + y
-        ts.create_tile(id)
-        ts.tile_set_texture(id, texture)
-        ts.tile_set_region(id, region)
 
-    ResourceSaver.save("res://terrain/terrain_tiles.tres", ts)
+    ResourceSaver.save("res://terrain/all_tiles.tres", ts)
