53a507e5f20d97b80ae7625d98d5d72bd906c8e3
Igor Mosyagin <i.mosyagin@lamoda.ru>
2019-04-30 01:11:37 +0300

    Add scythe action/animation. Fix #1

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -9,17 +9,28 @@ var alive = true
 
 func _ready():
 	$ScytheTimer.wait_time = scythe_cooldown
+	set_animation("down")
+
+func set_animation(animation_str):
+	$Sprite/AnimatedSprite.animation = animation_str
 
 func control(delta):
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		velocity += Vector2(speed, 0)
+		set_animation("right")
 	if Input.is_action_pressed("ui_left"):
 		velocity += Vector2(-speed, 0)
+		set_animation("left")
 	if Input.is_action_pressed("ui_down"):
 		velocity += Vector2(0, speed)
+		set_animation("down")
 	if Input.is_action_pressed("ui_up"):
 		velocity += Vector2(0, -speed)
+		set_animation("up")
+	if Input.is_action_pressed("ui_accept"):
+		$Sprite/AnimatedSprite.play()
+		$ScytheTimer.start()
 	
 	velocity = velocity.normalized() * speed
 
@@ -28,3 +39,7 @@ func _physics_process(delta):
 		return
 	control(delta)
 	move_and_slide(velocity)
+
+func _on_ScytheTimer_timeout():
+	$Sprite/AnimatedSprite.stop()
+	$Sprite/AnimatedSprite.frame = 0
