42e0c5acdb7d7c49f6e2ef09a95f8f34745d4172
Igor Mosyagin <i.mosyagin@lamoda.ru>
2019-04-29 22:37:08 +0300

    Add tweak variables to adjust camera limits

--- levels/Level.gd
+++ levels/Level.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
+export (int) var tweak_left
+export (int) var tweak_right
+export (int) var tweak_top
+export (int) var tweak_bottom
+
 func _ready():
 	set_camera_limits()
 	
 func set_camera_limits():
 	var map_limits = $Ground.get_used_rect()
 	var map_cellsize = $Ground.cell_size
-	$Player/Camera2D.limit_left = (map_limits.position.x + 3) * map_cellsize.x
-	$Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
-	$Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
-	$Player/Camera2D.limit_bottom = (map_limits.end.y - 4) * map_cellsize.y
+	$Player/Camera2D.limit_left = map_limits.position.x * map_cellsize.x + tweak_left
+	$Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x + tweak_right
+	$Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y + tweak_top
+	$Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y + tweak_bottom
